10234c2419e3b5abcc7ecf9f95540f5c296d678f
dyalick <98204933+NataCher@users.noreply.github.com>
2022-03-14 10:38:47 +0300

    Add files via upload

--- Игра Змейка/unit1.pas
+++ Игра Змейка/unit1.pas
@@ -5,18 +5,30 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls,
+  StdCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
     procedure FormCreate(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
+    procedure Image2Click(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
     procedure MenuItem1Click(Sender: TObject);
+    procedure MenuItem4Click(Sender: TObject);
   private
 
   public
@@ -37,7 +49,27 @@ begin
 
 end;
 
+procedure TForm1.Image1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Image2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Memo1Change(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.MenuItem1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem4Click(Sender: TObject);
 begin
 
 end;
